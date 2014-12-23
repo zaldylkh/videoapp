0f2458f49e843261140cbe718f0f6a053ed1c0e2
Guille RG <guille.rodriguez.gonzalez@gmail.com>
2014-12-23 13:36:17 +0100

    Actualizado script de minimizar, maximizar

--- scripts/myfunc.js
+++ scripts/myfunc.js
@@ -1,24 +1,22 @@
-$(document).on("ready", toggleMediaPlayer);
 function toggleMediaPlayer() {
-
-	$("#content").addClass("content-large");
-	$("#content").removeClass("content-small");
-	$("#footer").addClass("footer-small");
-	$("#footer").removeClass("footer-large");
-
-	$("#mostrar").on("click", function(){
-		$("#content").addClass("content-small");
-		$("#content").removeClass("content-large");
-		$("#footer").addClass("footer-large");
-		$("#footer").removeClass("footer-small");
-	});
-
-	$("#ocultar").on("click", function(){
-		$("#content").addClass("content-large");
-		$("#content").removeClass("content-small");
-		$("#footer").addClass("footer-small");
-		$("#footer").removeClass("footer-large");
+	$("#toggleMPlayer").on("click", function() {
+		var loadedClass = $("#content").attr('class');
+	
 		
-
+		if ( loadedClass == "content-large") {
+			$("#content").addClass("content-small");
+			$("#content").removeClass("content-large");
+			$("#footer").addClass("footer-large");
+			$("#footer").removeClass("footer-small");	
+			$("#toggleMPlayer").html('Minimizar');
+			
+			
+		} else {
+			$("#content").addClass("content-large");
+			$("#content").removeClass("content-small");
+			$("#footer").addClass("footer-small");
+			$("#footer").removeClass("footer-large");
+			$("#toggleMPlayer").html('Maximizar');
+		}
 	});
 }
